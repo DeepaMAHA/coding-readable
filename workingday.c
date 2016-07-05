a2f82e994eb8da0b8f73803459b56a21185bd1c0
DeepaMAHA <deepamaha.6633@gmail.com>
2016-07-05 08:40:56 -0700

    Create workingday.c

---
+++ workingday.c
@@ -0,0 +1,25 @@
+#include<stdio.h>
+#include<string.h>
+int workingday(char a);
+int main()
+{
+char a[20],f;
+scanf("%s",a);
+workingday(a);
+if(f==1)
+printf("True");
+else
+printf("false");
+return o;
+}
+int workingday(char x)
+{
+int f;
+if(x=='SUNDAY"||X=="Sunday")
+f=1;
+
+else
+f=0;
+return f;
+}
+
