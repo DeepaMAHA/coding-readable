8a251349347eb26fb1a9d41b7eb1925be7e4d4f7
DeepaMAHA <deepamaha.6633@gmail.com>
2016-07-05 09:23:09 -0700

    Create array.c

---
+++ array.c
@@ -0,0 +1,25 @@
+#include<stdio.h>
+int repeated(int x[100,int n);
+int main()
+{
+int a[100],i,n;
+scanf("%d",&n);
+for(i=0;i<n;i++)
+scanf("%d",&a[i]);
+repeated(a[i],n);
+if(f==1)
+printf("true");
+else
+printf("false");
+return 0;
+}
+int repeated(int a[i],int n)
+{
+for(i=0;i<n;i++)
+{
+if(a[i]==0)
+f=0;
+else
+f=1;
+return f;
+}
