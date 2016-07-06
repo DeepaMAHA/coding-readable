5f97e53a4a9eed0eebecbb37953cf955ea623070
DeepaMAHA <deepamaha.6633@gmail.com>
2016-07-06 11:55:03 +0530

    Create COIN.C

---
+++ COIN.C
@@ -0,0 +1,25 @@
+#include<stdio.h>
+int main()
+{
+int x,y,i,b,a[100],c=0;
+printf(no of coins");
+scanf("%d",&x);
+printf("coins value");
+for(i=0;i<n;i++)
+{
+scanf("%d",&a[i]);
+}
+scanf("%d",&y);
+while(y!=0)
+{
+b=0;
+b=s/a[n];
+if(b!=0)
+printf("%d%d",b,a[n]);
+c+=b;
+y=y%a[n];
+n--;
+return 0;
+}
+
+
