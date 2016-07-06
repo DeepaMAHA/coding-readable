512180a852b50a349bc6d5111c1f749bcc5eda72
DeepaMAHA <deepamaha.6633@gmail.com>
2016-07-06 12:04:01 +0530

    Create towrs.c

---
+++ towrs.c
@@ -0,0 +1,24 @@
+#include<stdio.h>
+void towers(int,char,char,char);
+int main()
+{
+int main()
+{
+int num;
+printf("number of disks");
+scanf("%d",&num);
+towers(num,'a','c','b');
+return 0;
+}
+void towers(int n,char x,char y,char z)
+{
+if(num==1)
+printf("\n Move disk 1 from peg %c to peg %c", x, z);
+towers(num-1,x,y,z);
+printf("\n Move disk 1 from peg %c to peg %c", x, z);
+towers(num-1,x,y,z);
+}
+
+}
+
+
