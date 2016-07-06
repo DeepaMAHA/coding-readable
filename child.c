55bcb32e539caa96f8cda6abeab218d4cf069e43
DeepaMAHA <deepamaha.6633@gmail.com>
2016-07-06 12:21:07 +0530

    Create child.c

---
+++ child.c
@@ -0,0 +1,18 @@
+#include<stdio.h>
+int main()
+{
+int n,x,pair,i;
+printf("enter the child");
+scanf("%d",&n);
+printf("pairs");
+scanf("%d",&pair);
+for(i=0;i<2*n;i++)
+{
+x=2*n-i;
+if(pair!=x)
+{
+printf("The pair is%d for days %d is %d\n",p,j++,p);
+}
+}
+return 0;
+}
