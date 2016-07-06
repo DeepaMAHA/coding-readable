4554236951e70ca532105e07e2921abfe5894f7d
DeepaMAHA <deepamaha.6633@gmail.com>
2016-07-06 10:26:20 +0530

    Create arrayindex.c

---
+++ arrayindex.c
@@ -0,0 +1,20 @@
+#include<stdioh>
+void index(int a[],int n);
+int main()
+{
+int a[100],n,i;
+scanf("%d",&n);
+for(i=0;i<n;i++)
+scan("%d",&a[i]);
+index(a[100],n);
+return 0;
+}
+void index(int a[],int n)
+{
+int i;
+for(i=0;i<n;i++)
+{
+if(a[i]==i])
+printf("%d",&a[i]);
+}
+}
