e72106c31c4416b3c0ab289d40524c4c3f3e1026
DeepaMAHA <deepamaha.6633@gmail.com>
2016-07-09 11:06:19 +0530

    Create binary.c

---
+++ binary.c
@@ -0,0 +1,17 @@
+#include<stdio.h>
+int main()
+{
+int n,ch[100],i;
+scanf("%d",&n);
+if(ch[0]=='000'&&ch[1]=='001'&&ch[2]=='010'&&ch[3]=='100'&&ch[4]=='110'&&ch[5]=='1110')
+{
+for(i=1;i<=3;i++)
+{
+printf("%d",ch);
+}
+else
+{
+printf("Invalid");
+}
+return 0;
+}
