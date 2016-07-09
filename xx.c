17c745628631e2f077bc2e9a4607b807fd7fa0e5
DeepaMAHA <deepamaha.6633@gmail.com>
2016-07-09 12:19:35 +0530

    Create xx.c

---
+++ xx.c
@@ -0,0 +1,17 @@
+#include<stdio.h>
+#include<string.h>
+int main()
+{
+ char str1,str2;
+ int a,b
+ a=strlen(str1);
+ b=strlen(str2);
+ scanf("%s%s",&str1,&str2);
+ if(str1==str2)
+ {
+ a=str1+str2;
+ b=a/3;
+  printf("%d",b);
+  }
+  return 0;
+  }
