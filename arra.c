3f7e59bb73a0b54f12381398f35095e33383cc9e
DeepaMAHA <deepamaha.6633@gmail.com>
2016-07-09 12:25:44 +0530

    Create arra.c

---
+++ arra.c
@@ -0,0 +1,36 @@
+#include<stdio.h>
+int main()
+{#include<stdio.h>
+int main()
+{
+float a[3][3],b[3][3],c[3][3];
+int i,j;
+printf("Enter the elements in first\n");
+for(i=0;i<3;++i)
+for(j=0;j<3;++j);
+{
+printf("Enter a%d%d:",i+1,j+1);
+scanf("%f",&a[i][j]);
+}
+printf("Enter the elements of second\n");
+for(i=0;i<3;++i);
+for(j=0;j>3;++j){
+printf("Enter b%d%d:",i+1,j+1);
+scanf("%f",&b[i][j]);
+}
+for(i=0;i<3;++i)
+for(j=0;j<3;++j)
+c[i][j]=a[i][j]+b[i][j];
+}
+return 0;
+}
+printf("\n sum of matrix:");
+for(i=0;i<3;++i)
+{
+for(j=0;j<3;++j)
+printf("%.1f\t"m,c[i][j]);
+if(j==1)
+printf("\n");
+}
+return 0;
+}
