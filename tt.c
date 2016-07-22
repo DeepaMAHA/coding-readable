541e1e537ac1c52219414d8d9b65545fb1fd44df
DeepaMAHA <deepamaha.6633@gmail.com>
2016-07-22 14:46:15 +0530

    Create tt.c

---
+++ tt.c
@@ -0,0 +1,29 @@
+#include <stdio.h>
+#include <string.h>
+#include <ctype.h>
+
+	void main() {
+	  char text[100];
+	  printf("Please enter strings:");
+
+	   scanf("%s",text);
+
+	  while (strlen(text) >= 1){
+
+
+
+	   printf("The string is : %s\n",text);
+
+
+
+
+
+
+gets(text);
+
+
+	  }
+
+
+
+	}
