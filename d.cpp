bf280d203d4a4d071a321b1a116e0f8cd666a329
DeepaMAHA <deepamaha.6633@gmail.com>
2016-07-28 14:32:36 +0530

    Create d.cpp

---
+++ d.cpp
@@ -0,0 +1,31 @@
+#include<iostream>
+using namespace std;
+int checkTwo(int n);
+int b1(int k);
+int b2(int k);
+int main()
+{
+int k;
+cout<<k;
+cin>>k;
+bye1(k);
+bye2(k);
+return 1;
+}
+int b1(int k)
+{
+	int a;
+	for(int i=0;i<k;i++)
+	{
+		a=k-i;
+		a=a/2;
+		a=a+i;
+		if(a%2==0)
+		{
+			if(checkTwo(a))
+			{
+				cout<<"the players of bye given based on first condition(2^n) is:"<<i<<endl;
+				return 0;
+			}
+		}
+	}
